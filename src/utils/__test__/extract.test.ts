af9ae7a790edf3f447847d9ead3015a8877418df
feiYu <feiyucode@gmail.com>
2024-12-20 16:51:58 +0800

    feat: extract()

---
+++ src/utils/__test__/extract.test.ts
@@ -0,0 +1,32 @@
+import { test, expect } from 'vitest'
+import { extract } from '../index.js'
+
+test('extract()', () => {
+	const prototype = { c: 3 }
+	type Data = { a: number; b: number; c: number }
+	const target = { a: 1, b: 2 } as unknown as Data
+	Object.setPrototypeOf(target, prototype)
+	const result = extract<Data, (keyof Data)[]>(target, ['a', 'b', 'c'])
+	expect(result).toStrictEqual({ a: 1, b: 2, c: 3 })
+})
+
+test('extract() not prototype', () => {
+	const prototype = { c: 3 }
+	type Data = { a: number; b: number; c: number }
+	const target = { a: 1, b: 2 } as unknown as Data
+	Object.setPrototypeOf(target, prototype)
+	const result = extract<Data, (keyof Data)[]>(target, ['a', 'b', 'c'], { containPrototype: false })
+	expect(result).toStrictEqual({ a: 1, b: 2, c: void 0 })
+})
+
+test('extract() not prototype notValueWriteUndefined set false', () => {
+	const prototype = { c: 3 }
+	type Data = { a: number; b: number; c: number }
+	const target = { a: 1, b: 2 } as unknown as Data
+	Object.setPrototypeOf(target, prototype)
+	const result = extract<Data, (keyof Data)[]>(target, ['a', 'b', 'c'], {
+		containPrototype: false,
+		notValueWriteUndefined: false
+	})
+	expect(result).toStrictEqual({ a: 1, b: 2 })
+})
---
+++ src/utils/extract/index.ts
@@ -0,0 +1,38 @@
+export interface Options {
+	/** 是否包含原型链上的属性, 默认为 true */
+	containPrototype?: boolean
+	/** 当获取不到数据时写入 undefined, 若不写入则结构中不会包含该字段, 默认为 true */
+	notValueWriteUndefined?: boolean
+}
+
+/**
+ * 提取对象指定字段
+ * @param target 目标
+ * @param keys 提取的字段数组
+ * @param options 配置选项
+ * @returns 提取后新的数据
+ */
+export const extract = <T1 extends object, T2 extends (keyof T1)[]>(
+	target: T1,
+	keys: T2,
+	options?: Options
+): Pick<T1, T2[number]> => {
+	type Data = Pick<T1, T2[number]>
+	const data: Data = {} as Data
+	const { notValueWriteUndefined = true, containPrototype = true } = options || {}
+	if (containPrototype) {
+		keys.forEach((key) => {
+			data[key] = target[key]
+		})
+	} else {
+		keys.forEach((key) => {
+			if (Object.hasOwn(target, key)) {
+				data[key] = target[key]
+			} else if (notValueWriteUndefined) {
+				data[key] = void 0
+			}
+		})
+	}
+
+	return data
+}
