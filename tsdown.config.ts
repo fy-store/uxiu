2269a5135ef8c0fa9e1c9a72bc7980b2a055cf5d
feiyu <feiyucode@gmail.com>
2026-06-14 01:38:26 +0800

    fix: 修复构建和测试问题

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,9 +4,10 @@ export default defineConfig({
 	entry: {
 		index: './src/index.ts',
 		utils: './src/utils/index.ts',
-		dependentUtils: './src/dependentUtils/index.ts'
+		dependence: './src/dependence/index.ts',
+		node: './src/node/index.ts'
 	},
-    outDir: './dist',
+	outDir: './dist',
 	target: 'es2020',
 	platform: 'node',
 	// exports: true,
