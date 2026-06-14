984b70765c79192aab6dd58a9280c92d41e534c1
feiyu <feiyucode@gmail.com>
2026-06-14 17:14:20 +0800

    feat: 合并添加的方法

--- packages/core/src/dependence/index.ts
+++ packages/core/src/dependence/index.ts
@@ -1,3 +1,5 @@
 export * from './everydayTask/index.js'
 export * from './random/index.js'
 export * from './sleep/index.js'
+export * from './debounce/index.js'
+export * from './throttle/index.js'
